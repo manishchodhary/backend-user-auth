e768b29b79869ad140133c654e073b8d0d0b5ed5
Manish choudhary <manishjee10102006@gmail.com>
2026-09-02 00:31:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./backend-project/src/app.js";
 
 import coonectDB from "./backend-project/src/config/db.js";
-coonectDB()
+//coonectDB()
 const PORT = 5000
 app.listen(PORT,()=>{
     console.log(`Server is connected on PORT ${PORT}`);
